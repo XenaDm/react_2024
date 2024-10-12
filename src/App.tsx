eab0474f8edfa0969a92e4e228a004e90e153fd5
HTML Project <ksiushabliz@gmail.com>
2024-10-12 17:17:38 +0300

    lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
 import './App.css';
+import Character from "./components/character/Character";
+import Users from "./components/users/users";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App: FC = () => {
+    return (
+        <>
+            <Character name={'Bart'}
+            image={"https://static.simpsonswiki.com/images/thumb/6/65/Bart_Simpson.png/300px-Bart_Simpson.png"}
+        />
+
+    <Character name={'Homer'}
+    image={"https://static.simpsonswiki.com/images/b/bd/Homer_Simpson.png"}
+    />
+    <Character name={'Marge'}
+    image={"https://upload.wikimedia.org/wikipedia/uk/0/0b/Marge_Simpson.png"}>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, cum dicta dolor dolore doloremque exercitationem minus necessitatibus nostrum numquam officia officiis reiciendis repellendus repudiandae similique sit tempore ut veritatis voluptatibus?
+    </Character>
+            <hr/>
+            <Users/>
+</>
+);
 }
+
 
 export default App;
