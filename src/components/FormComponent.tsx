3b1f31f10a5b3a64e8acc68a16a920f5a8addc01
HTML Project <ksiushabliz@gmail.com>
2024-11-07 19:02:08 +0200

    useForm joi(pattern - undefined bags)(ще не пофіксили багі внутрішньосистемні)(2\3 video)

--- src/components/FormComponent.tsx
+++ src/components/FormComponent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {useForm} from "react-hook-form";
+import {joiResolver} from "@hookform/resolvers/joi";
+import {userValidator} from "../validators/user.validator";
 
 
 type FormProps = {
@@ -16,7 +18,7 @@ const FormComponent = () => {
             errors,
             isValid
         }
-    } = useForm<FormProps>({mode:'all'});
+    } = useForm<FormProps>({mode:'all', resolver: joiResolver(userValidator)});
 
     const customHandler = (dataFromForm: FormProps) => {
         console.log(dataFromForm);
@@ -25,21 +27,21 @@ const FormComponent = () => {
     };
     return (
             <form onSubmit={handleSubmit(customHandler)}>
-                <div><label><input type="text" placeholder={'username'} {...register('username', {
-                    required: {value: true, message: "username is required"},
-                })}/>
+                <div>
+                    <label>
+                        <input type="text" placeholder={'username'} {...register}/>
                     {errors.username && <div>{errors.username.message}</div>}
-                </label></div>
-                <div><label><input type="text" placeholder={'password'} {...register('password', {
-                    minLength: {value: 3, message: 'too short'},
-                    maxLength: {value: 6, message: 'too long'},
-                })}/>
+                </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="text" placeholder={'password'} {...register}/>
                     {errors.password && <div>{errors.password.message}</div>}
-                </label></div>
-                <div><label><input type="number" placeholder={'age'} {...register('age', {
-                    min: {value: 0, message: 'too young'},
-                    max: {value: 123, message: 'too old'}
-                })}/>
+                </label>
+                </div>
+                <div>
+                    <label>
+                        <input type="number" placeholder={'age'} {...register}/>
                     {errors.age && <div>{errors.age.message}</div>}
                 </label></div>
                     <button disabled={!isValid}>save</button>
---
+++ src/validators/user.validator.ts
@@ -0,0 +1,22 @@
+import Joi from "joi";
+
+export const userValidator = Joi.object({
+username: Joi.string()
+    // .required()
+    .pattern(new RegExp(/^[a-z A-Z0-9]{3,30}$/)),
+
+    password: Joi.string()
+        .min(3)
+        .max(6)
+        .messages({
+            "string.min":"password must be at least 3 char",
+            "string.max":"password can`t be greater then 6 char"
+        }),
+    age: Joi.number()
+        .min(1)
+        .max(121)
+        .messages({
+            "number.min":"age must be at least 1",
+            "number.max":"age cant be greater than 121"
+        }),
+});
