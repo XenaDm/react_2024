0159b1c4e646987d6bb2f7ed08b73c144639a274
HTML Project <ksiushabliz@gmail.com>
2024-11-12 21:45:03 +0200

    lesson 7(hooks), 4/4 of lection, Lifecykle methods, class constuctor

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
-import TonyStark from "./TonyStark";
-import ColonelMamba from "./ColonelMamba";
-import IronSuit from "./IronSuit";
+import React, {Component} from 'react';
 
+type PropsType ={}
+type StateType ={
+    users: any[];
+    posts: string[];
+    comments: string[];
+}
+class App extends Component<PropsType, StateType> {
 
-function App () {
+    constructor(props: any) {
+        super(props);
+        this.state ={users: [], posts: [], comments: []}
+    }
 
-return (
-        <div>
-            <IronSuit><TonyStark/></IronSuit>
-            <IronSuit><ColonelMamba/></IronSuit>
-        </div>
-    );
+componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(value => value.json())
+            .then(value => {
+                this.setState({users: value})
+            })
+}
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.users.map(user => (<div>{user.name}</div>))
+                }
+            </div>
+        );
+    }
 }
 
 export default App;
